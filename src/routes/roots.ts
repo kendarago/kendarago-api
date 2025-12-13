fde596c1c3817a2c38bf777dee0b660992966f3f
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-12-13 20:25:12 +0700

    refactor (roots) : added descrtiption

--- src/routes/roots.ts
+++ src/routes/roots.ts
@@ -3,6 +3,8 @@ import { Hono } from "hono";
 export const rootsRoute = new Hono().get("/", (c) => {
   return c.json({
     message: "Kendarago API",
+    description:
+      "A backend API for Kendarago, a web-based application designed to facilitate vehicle rentals. Built with Bun, Hono, Prisma, and PostgreSQL.",
     path: {
       schools: "/vehicles",
     },
