0b070635325039dae31676e08c6ff7fb424d2429
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-12-13 15:12:26 +0700

    feat: create route vehicles

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,32 @@
 import { Hono } from "hono";
+import { Hono } from "hono";
 
 const app = new Hono();
 
+const vehicles = [{}];
+
 app.get("/", (c) => {
-  return c.text("Hello Hono!");
+  return c.json({
+    message: "Kendarago API",
+  });
+});
+
+app.get("/vehicles", (c) => {
+  return c.json(vehicles);
+});
+
+app.get("/vehicles/:id", (c) => {
+  const id = Number(c.req.param("id"));
+
+  // const vehicle = vehicles.find((vehicle) => {
+  //   return vehicle.id === id;
+  // });
+
+  if (!vehicles) {
+    return c.notFound();
+  }
+
+  return c.json(vehicles);
 });
 
 export default app;
