e1a7075f9bfb1d55b20adab3647c50d6a0dd01ea
Mochamad Irvan <mchmdirvaan@gmail.com>
2025-12-15 07:29:52 +0700

    feat: create rental companies data

---
+++ prisma/data/rental-companies.ts
@@ -0,0 +1,9 @@
+export type RentalCompanies = {
+  name: string;
+  address: string;
+  operatingHours: string;
+  contact: number;
+  imageUrl?: string;
+};
+
+export const dataRentalCompanies: RentalCompanies[] = [];
