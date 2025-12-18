54ce72b7e25ca29fc88d624e6ca9de4df7803382
Muhammad Rizki Kurnia Putra <muhammadrizki05@gmail.com>
2025-12-18 17:48:21 +0700

    feat : user route and schema

---
+++ src/routes/user.ts
@@ -0,0 +1,66 @@
+import { createRoute, OpenAPIHono } from "@hono/zod-openapi";
+import {
+  PublicUserSchema,
+  UsersSchema,
+  UsersIdSchema,
+} from "../module/user-schema";
+import { prisma } from "../lib/prisma";
+
+export const userRoute = new OpenAPIHono();
+
+userRoute.openapi(
+  createRoute({
+    method: "get",
+    path: "/",
+    responses: {
+      200: {
+        content: { "application/json": { schema: UsersSchema } },
+        description: "Get all users",
+      },
+    },
+  }),
+  async (c) => {
+    const users = await prisma.user.findMany({
+      omit: {
+        email: true,
+      },
+    });
+
+    return c.json(users, 200);
+  }
+);
+
+userRoute.openapi(
+  createRoute({
+    method: "get",
+    path: "/{id}",
+    request: {
+      params: UsersIdSchema,
+    },
+    responses: {
+      200: {
+        content: { "application/json": { schema: PublicUserSchema } },
+        description: "Get all users",
+      },
+      404: {
+        description: "Not found",
+      },
+    },
+  }),
+  async (c) => {
+    const { id } = c.req.valid("param");
+
+    const user = await prisma.user.findUnique({
+      where: { id },
+      omit: {
+        email: true,
+      },
+    });
+
+    if (!user) {
+      return c.json({ message: "User not found" }, 404);
+    }
+
+    return c.json(user, 200);
+  }
+);
