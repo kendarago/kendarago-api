0203d6814c06cc655d44234ca0e64879e2532417
Muhammad Rizki Kurnia Putra <muhammadrizki05@gmail.com>
2025-12-18 20:50:59 +0700

    feat:s ign token

---
+++ src/lib/token.ts
@@ -0,0 +1,19 @@
+import { sign, verify } from "hono/jwt";
+
+type Payload = {
+  sub: string;
+};
+
+export async function signToken(userId: string) {
+  const payload: Payload = {
+    sub: userId, // subject = user id
+  };
+  const secret = String(process.env.TOKEN_SECRET_KEY);
+  return await sign(payload, secret);
+}
+
+export async function verifyToken(token: string) {
+  const secret = String(process.env.TOKEN_SECRET_KEY);
+
+  return (await verify(token, secret)) as Payload;
+}
